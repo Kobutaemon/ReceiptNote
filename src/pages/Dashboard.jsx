24f47ebca48c6aee2766d92b0f90252b83b3176f
Kobutaemon <isotoa1219@gmail.com>
2025-06-18 19:18:24 +0900

    フッター追加

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import CardList from "../components/CardList";
+import { NavLink } from "react-router-dom";
 
 function Dashboard() {
   return (
-    <main>
-      <CardList />
-    </main>
+    <div>
+      <header>
+        <h1 className="text-3xl text-center pt-4 font-bold">ReceiptNote</h1>
+      </header>
+      <main>
+        <CardList />
+      </main>
+      <footer>
+        <p className="text-center fixed bottom-4 right-[50%] translate-x-[50%]">
+          &copy; 2025 Kobutaemon All Right Reserved.
+        </p>
+      </footer>
+    </div>
   );
 }
 
