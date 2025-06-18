97cd0424669849de921a8c27dd5ae9bf28127f2e
Kobutaemon <ceti1241010@gn.iwasaki.ac.jp>
2025-06-18 14:28:02 +0900

    デザイン、改行の調整

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -25,56 +25,40 @@ function Sidebar() {
 
   return (
     <div
-      className={`flex gap-4 fixed right-0 transition-all duration-300 ${
+      className={`flex gap-4 fixed right-0 transition-all duration-300 z-10 ${
         isMenuOpen ? "translate-x-0" : "translate-x-[80%]"
       }`}
     >
       <button
-        className="w-10 h-10 flex items-center justify-center cursor-pointer hover:bg-gray-200 rounded mt-5 transition-colors duration-200"
+        className="w-10 h-10 flex items-center justify-center cursor-pointer bg-gray-200 rounded mt-5 transition-colors duration-200 border border-black"
         onClick={menuClick}
         aria-label="メニューを開閉する"
       >
         {isMenuOpen ? <X size={20} /> : <PanelRight size={20} />}
       </button>
       <nav
-        className="bg-gray-100 h-screen pt-10 px-5 transition-transform duration-300 ease-in-out"
+        className="bg-gray-100 h-screen pt-10 px-5 transition-transform duration-300 ease-in-out border-l border-black"
         // isMenuOpenがfalseの時にメニューがフォーカスされないようにするなら下のコメントアウトを解除
         // style={{ visibility: isMenuOpen ? "visible" : "hidden" }}
       >
         <ul className="flex flex-col gap-4 text-lg">
           <li>
-            <NavLink
-              to="/"
-              style={NavStyle}
-              onClick={handleNavClick}
-            >
+            <NavLink to="/" style={NavStyle} onClick={handleNavClick}>
               ダッシュボード
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to="/bicycle"
-              style={NavStyle}
-              onClick={handleNavClick}
-            >
+            <NavLink to="/bicycle" style={NavStyle} onClick={handleNavClick}>
               駐輪代
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to="/shopping"
-              style={NavStyle}
-              onClick={handleNavClick}
-            >
+            <NavLink to="/shopping" style={NavStyle} onClick={handleNavClick}>
               買い物代
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to="/something"
-              style={NavStyle}
-              onClick={handleNavClick}
-            >
+            <NavLink to="/something" style={NavStyle} onClick={handleNavClick}>
               その他
             </NavLink>
           </li>
