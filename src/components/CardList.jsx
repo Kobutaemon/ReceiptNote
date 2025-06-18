bb5760e7b2cf1b8bb13343cd071bf3e089a11d0c
Kobutaemon <isotoa1219@gmail.com>
2025-06-18 17:19:20 +0900

    Cardコンポーネントの変更に合わせたプロップス渡しの変更

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -4,9 +4,24 @@ import Card from "./Card";
 function CardList() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6 mt-10">
-      <Card svgName="bicycle" cardTitle="駐輪代" cardTotal="¥2,000" />
-      <Card svgName="shopping" cardTitle="買い物代" cardTotal="¥15,000" />
-      <Card svgName="something" cardTitle="その他" cardTotal="¥3,500" />
+      <Card
+        svgName="bicycle"
+        svgColor="blue"
+        cardTitle="駐輪代"
+        cardTotal="2,000"
+      />
+      <Card
+        svgName="shopping"
+        svgColor="green"
+        cardTitle="買い物代"
+        cardTotal="15,000"
+      />
+      <Card
+        svgName="something"
+        svgColor="gray"
+        cardTitle="その他"
+        cardTotal="3,500"
+      />
     </div>
   );
 }
