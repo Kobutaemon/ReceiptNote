b6ccf59a1c69dd95cff8ad9da2db421deb35d849
Kobutaemon <isotoa1219@gmail.com>
2025-06-24 17:21:43 +0900

    カラーマップにbtnColorMapを追加。

---
+++ src/utils/colorMap.js
@@ -0,0 +1,21 @@
+// プロジェクト共通のsvgカラーマップ
+export const svgColorMap = {
+  blue: {
+    bg: "bg-blue-100",
+    text: "text-blue-500",
+  },
+  green: {
+    bg: "bg-green-100",
+    text: "text-green-500",
+  },
+  gray: {
+    bg: "bg-gray-100",
+    text: "text-black",
+  },
+};
+
+export const btnColorMap = {
+  blue: "bg-blue-500",
+  green: "bg-green-500",
+  gray: "bg-gray-500",
+};
