2e920edcb7874b6471301a919d0ef2ba1467b0be
Kobutaemon <isotoa1219@gmail.com>
2025-06-24 17:27:30 +0900

    ptの変更

--- src/pages/Bicycle.jsx
+++ src/pages/Bicycle.jsx
@@ -4,7 +4,7 @@ function Bicycle() {
   return (
     <div>
       <div>
-        <h2 className="text-3xl text-center font-bold pt-4">駐輪代 登録</h2>
+        <h2 className="text-3xl text-center font-bold pt-8">駐輪代 登録</h2>
       </div>
       <Register btnColor="blue" />
     </div>
