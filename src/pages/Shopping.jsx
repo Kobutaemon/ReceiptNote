7d3b00e5675985547517fd2d97c81a417e4325e0
Kobutaemon <isotoa1219@gmail.com>
2025-06-24 17:28:00 +0900

    それぞれの登録ページを作成

--- src/pages/Shopping.jsx
+++ src/pages/Shopping.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Register from "../components/Register";
 
 function Shopping() {
-  return <div>Shopping</div>;
+  return (
+    <div>
+      <div>
+        <h2 className="text-3xl text-center font-bold pt-8">買い物代 登録</h2>
+      </div>
+      <Register btnColor="green" />
+    </div>
+  );
 }
 
 export default Shopping;
