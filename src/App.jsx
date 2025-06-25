1c748c3548030522bcc585bcc4ab14c40f412b9e
Kobutaemon <ceti1241010@gn.iwasaki.ac.jp>
2025-06-25 14:25:43 +0900

    Supabaseの認証機能を利用したログイン機能を実装。 また、登録ページのリファクタリングに合わせたRouteの変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,87 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
-import Bicycle from "./pages/Bicycle.jsx";
-import Shopping from "./pages/Shopping.jsx";
-import Something from "./pages/Something.jsx";
+
+// 以下Supabaseのためのimport
+import { useState, useEffect } from "react";
+import { supabase } from "./lib/supabaseClient.js";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import Register from "./components/Register.jsx";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Sidebar />
-      <div className="bg-gray-100 h-screen">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/bicycle" element={<Bicycle />} />
-          <Route path="/shopping" element={<Shopping />} />
-          <Route path="/something" element={<Something />} />
-        </Routes>
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!session) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="w-full max-w-md p-8">
+          <Auth
+            supabaseClient={supabase}
+            // appearanceプロパティでテーマを適用します
+            appearance={{ theme: ThemeSupa }}
+            providers={[]}
+            theme="dark"
+          />
+        </div>
       </div>
-    </BrowserRouter>
-  );
+    );
+  } else {
+    return (
+      <BrowserRouter>
+        <Sidebar />
+        <div className="bg-gray-100 h-screen">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route
+              path="/bicycle"
+              element={
+                <Register
+                  btnColor="blue"
+                  category="駐輪代"
+                  user={session.user}
+                />
+              }
+            />
+            <Route
+              path="/shopping"
+              element={
+                <Register
+                  btnColor="green"
+                  category="買い物代"
+                  user={session.user}
+                />
+              }
+            />
+            <Route
+              path="/something"
+              element={
+                <Register
+                  btnColor="gray"
+                  category="その他"
+                  user={session.user}
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
