f46a4e5dc948b61af7dff18b8aa9754d763c8a06
Kobutaemon <ceti1241010@gn.iwasaki.ac.jp>
2025-06-25 14:03:03 +0900

    supabase用のインポートやlibファイル作成

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+// 環境変数からURLとキーを取得
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+// Supabaseクライアントを作成してエクスポート
+export const supabase = createClient(supabaseUrl, supabaseKey);
